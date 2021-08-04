bbb7a7ff159934a41bf8c2a4ffcffad951d3f9ab
rkdudkey <rattikarn.dudley@gmail.com>
2021-08-03 20:10:26 -0400

    add team planner project

---
+++ Phase_1/Team_Budget_Planner/storage.js
@@ -0,0 +1,61 @@
+
+function addData() {
+
+    //get data from the input box
+    var client = document.getElementById("client").value;
+    var project = document.getElementById("project").value;
+    var budget = document.getElementById("budget").value;
+
+    const dataObj = {
+        client: client,
+        project: project,
+        budget: budget
+    }
+
+    //get the item from the localstorage if null get empty array 
+    var annualData = JSON.parse(localStorage.getItem("annualData") || "[]");
+
+    //push the data object 
+    annualData.push(dataObj);
+
+    // Saving the data into localStorage
+    localStorage.setItem("annualData", JSON.stringify(annualData));
+
+    //tell user data is added
+    document.getElementById("displayAdd").innerHTML = "The data is already added";
+
+}
+
+function clearData() {
+    var client = document.getElementById("client");
+    var project = document.getElementById("project");
+    var budget = document.getElementById("budget");
+    var displayAdd = document.getElementById("displayAdd");
+
+    //empty input box
+    client.value ="";
+    project.value ="";
+    budget.value ="";
+    displayAdd.innerHTML ="";
+
+}
+
+function displayData() {
+    let annualData = localStorage.getItem("annualData");
+    let dataJson = JSON.parse(annualData);
+
+    var tableContent=""
+    var startTable ="<table class=\"container-table\"><tr><th>Client Name</th><th>Project Name</th><th>Budget </th></tr>"
+    
+    dataJson.forEach(item => {
+        tableContent += `<tr>
+                        <td> ${item.client} </td>
+                        <td> ${item.project} </td>
+                        <td> ${item.budget} </td>
+                        </tr>`
+    });
+    
+    var endTable="</table>"
+    tableContent = startTable+tableContent+endTable
+    document.getElementById("main").innerHTML=tableContent;
+}
